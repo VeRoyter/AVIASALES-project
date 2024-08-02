122ae11d635457b7a9c075864d4756ae9b91f0c8
VeRoyter <137034950+VeRoyter@users.noreply.github.com>
2024-08-02 14:50:22 +1000

    Add files via upload

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,28 @@
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/styles'
+
+import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import { mdi } from 'vuetify/iconsets/mdi'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+       mdi,
+    },
+  },
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#E1ECEB',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+})
